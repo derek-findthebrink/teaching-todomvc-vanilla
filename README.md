ff0c6b6667fc1d24d4e4426caad6b0add18c4db2
derek-findthebrink <info@findthebrink.com>
2020-07-15 16:19:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-mgzfxz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-mgzfxz)
---
+++ index.js
@@ -0,0 +1,5 @@
+// Import stylesheets
+import "./style.css";
+
+console.log('hola')
+console.log('furk nurr ')
